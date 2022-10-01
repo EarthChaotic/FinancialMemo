fbebc78915f4ec13f22a5ccbd6ca1c3d4ffdc245
EarthChaotic <jeraponteesup@gmail.com>
2022-10-01 16:41:09 +0700

    Support Screen

--- Screens/Styles.js
+++ Screens/Styles.js
@@ -1,69 +1,126 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { color } from "react-native-reanimated";
 
 const Styles = StyleSheet.create({
-    headericon: {
-        width: 40,
-        height: 40,
-        marginLeft:10
-      },
-      Homecontainer: {
-        height: 80,
-        elevation: 3,
-        borderColor: "gray",
-        borderRadius: 5,
-        flexDirection: "row",
-        marginLeft: 20,
-        marginTop: 10,
-      },
-      HomedataContainer: {
-        flex: 1,
-      },
-      Homethumbnail: {
-        width: 50,
-        height: 50,
-        marginTop: 10,
-      },
-      dataContent: {
-        marginTop: 5,
-        marginLeft: 5,
-      },
-      titleFin: {
-        color: "#18B515",
-        fontSize: 17,
-        fontWeight: "bold",
-      },
-      titleWal: {
-        color: "#E2D96B",
-        fontSize: 17,
-        fontWeight: "bold",
-      },
-      titleSum: {
-        color: "#6B99C3",
-        fontSize: 17,
-        fontWeight: "bold",
-      },
-      titleSup: {
-        color: "#E538AE",
-        fontSize: 17,
-        fontWeight: "bold",
-      },
-      detail: {
-        fontSize: 13,
-        color: "#16354D",
-        fontWeight: "bold",
-      },
-      floattouch:{
-        borderWidth: 2,
-        borderColor: 'rgba(0,0,0,0.2)',
-        width: 50,
-        height: 50,
-        backgroundColor: '#fE0',
-        borderRadius: 100,
-        bottom:10,
-        right:10,
-        position: 'absolute',
-      }
-})
+  headericon: {
+    width: 40,
+    height: 40,
+    marginLeft: 10,
+  },
+  Homecontainer: {
+    height: 80,
+    elevation: 3,
+    borderColor: "gray",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  HomedataContainer: {
+    flex: 1,
+  },
+  Homethumbnail: {
+    width: 50,
+    height: 50,
+    marginTop: 10,
+  },
+  dataContent: {
+    marginTop: 5,
+    marginLeft: 5,
+  },
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  titleFin: {
+    color: "#18B515",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  titleWal: {
+    color: "#E2D96B",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  titleSum: {
+    color: "#6B99C3",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  titleSup: {
+    color: "#E538AE",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  detail: {
+    fontSize: 13,
+    color: "#16354D",
+    fontWeight: "bold",
+  },
+  floattouch: {
+    borderWidth: 2,
+    borderColor: "rgba(0,0,0,0.2)",
+    width: 50,
+    height: 50,
+    backgroundColor: "#fE0",
+    borderRadius: 100,
+    bottom: 10,
+    right: 10,
+    position: "absolute",
+  },
+  SupportSubject: {
+    width: 300,
+    height: 45,
+    marginBottom: 10,
+    fontSize: 20,
+    textAlign:"left",
+  },
+  SupportTextInp: {
+    marginTop:10,
+    width: 300,
+    maxHeight:500,
+    fontSize: 20,
+    textAlign:"left"
+  },
+  SendPlace: {
+    elevation: 3,
+    borderColor: "gray",
+    borderRadius: 5,
+    flexDirection: "row",
+    marginLeft: 20,
+    marginTop: 20,
+    justifyContent:"flex-end"
+  },
+  SendTouch:{
+    marginRight:10,
+    backgroundColor:'#E538AE',
+    padding:5,
+    borderRadius: 5,
+  },
+  CancelTouch:{
+    marginRight:10,
+    backgroundColor:'#8D9DA9',
+    padding:5,
+    borderRadius: 5,
+  },
+  SendText:{
+    fontSize:15,
+    fontWeight:'600',
+    color:'white'
+  },
+  CancelText:{
+    fontSize:15,
+    fontWeight:'600',
+    color:'white'
+  },
+  testcontain:{
+    flex:1,
+    alignItems:'center',
+    backgroundColor:'#16354D',
+    justifyContent:'center'
+  }
+});
 
-export default Styles
+export default Styles;
--- Screens/SupportScreen.js
+++ Screens/SupportScreen.js
@@ -1,24 +1,29 @@
 import React from "react";
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import {
-  HeaderButtons,
-} from "react-navigation-header-buttons";
+import { Text, View, TouchableOpacity, Image, TextInput } from "react-native";
+import { HeaderButtons } from "react-navigation-header-buttons";
 import Styles from "./Styles";
+const ItemSeparatorView = () => {
+  return (
+    // Flat List Item Separator
+    <View
+      style={{
+        height: 0.5,
+        width: "100%",
+        backgroundColor: "#C8C8C8",
+      }}
+    />
+  );
+};
 
 const SupportScreen = ({ navigation }) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
-        <HeaderButtons >
+        <HeaderButtons>
           <TouchableOpacity onPress={() => navigation.openDrawer()}>
             <Image
-            source={require("../Screens/Pics/MemoHeader.png")}
-            style={Styles.headericon}
+              source={require("../Screens/Pics/MemoHeader.png")}
+              style={Styles.headericon}
             />
           </TouchableOpacity>
         </HeaderButtons>
@@ -28,7 +33,26 @@ const SupportScreen = ({ navigation }) => {
 
   return (
     <View>
-        <Text>Support จ้า</Text>
+      <View style={Styles.Homecontainer}>
+        <View style={Styles.dataContent}>
+          <TextInput style={Styles.SupportSubject} placeholder="Subject" />
+          <ItemSeparatorView />
+          <TextInput
+            style={Styles.SupportTextInp}
+            placeholder="Input Text Here"
+          />
+        </View>
+      </View>
+      <View>
+        <View style={Styles.SendPlace}>
+            <TouchableOpacity style={Styles.SendTouch} onPress={() => navigation.navigate("HOME")}>
+                <Text style={Styles.SendText}>Send</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={Styles.CancelTouch} onPress={() => navigation.navigate("HOME")}>
+                <Text style={Styles.CancelText}>Cancel</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
